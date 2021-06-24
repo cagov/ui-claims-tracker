2704dcde540466b06dc98baeffc84ec6810398b7
Rocket ! <rocketnova@users.noreply.github.com>
2021-06-24 12:35:10 -0700

    Refactor claim details into separate helper functions.

--- tests/utils/getScenarioContent.test.tsx
+++ tests/utils/getScenarioContent.test.tsx
@@ -1,4 +1,6 @@
 import getScenarioContent, {
+  buildBenefitYear,
+  getClaimDetails,
   getScenario,
   getClaimStatusDescription,
   mapProgramType,
@@ -19,10 +21,6 @@ const baseNoPendingScenario = { hasPendingWeeks: false }
  */
 
 // Test getScenarioContent()
-// describe('Retrieving the scenario content', () => {
-//   // Test Claim Details
-//   it('returns the correct programType and extensionType', () => {})
-// })
 
 // Test getClaimStatusDescripton()
 describe('Getting the Claim Status description', () => {
@@ -131,5 +129,42 @@ describe('Converting ProgramType to user-facing strings', () => {
     expect(() => {
       mapProgramType('unknown')
     }).toThrowError('Unknown Program Type')
+  })
+})
+
+// Test buildBenefitYear()
+describe('Constructing the benefit year', () => {
+  it('results in a date range string', () => {
+    const range = buildBenefitYear('foo', 'bar')
+    expect(range).toBe('foo - bar')
+  })
+})
+
+// Test getClaimDetails()
+describe('Constructing the Claim Details object', () => {
+  it('returns all the expected values', () => {
+    // Mock ClaimDetailsResults
+    const rawDetails = {
+      programType: 'UI',
+      benefitYearStartDate: '5/21/21',
+      benefitYearEndDate: '5/20/22',
+      claimBalance: '$100',
+      weeklyBenefitAmount: '$25',
+      lastPaymentIssued: '3/12/21',
+      lastPaymentAmount: '$10',
+      monetaryStatus: 'active',
+    }
+
+    // Expected results
+    const expected = {
+      programType: 'claim-details:program-type.ui',
+      benefitYear: '5/21/21 - 5/20/22',
+      claimBalance: rawDetails.claimBalance,
+      weeklyBenefitAmount: rawDetails.weeklyBenefitAmount,
+      lastPaymentIssued: rawDetails.lastPaymentIssued,
+      extensionType: '',
+    }
+    const claimDetails = getClaimDetails(rawDetails)
+    expect(claimDetails).toStrictEqual(expected)
   })
 })
